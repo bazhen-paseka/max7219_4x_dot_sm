40a62ed4604ce2c5d2eb4d5b19173b835b967f56
Бажен Левковець <bazhen.paseka@gmail.com>
2019-09-09 13:58:02 +0300

    add enum

--- max7219_4x_dot_sm.c
+++ max7219_4x_dot_sm.c
@@ -5,7 +5,26 @@
 #define BYTE_IN_SPI_PACKAGE		8
 #define SPI_PACKAGE_TIMEOUT		1
 
-	uint8_t myTrans[8]; // 1 - for  razryad or Adres; 2- znachenie
+
+typedef enum
+{
+	ADDR_NO_OP 			= 0x00 ,
+	ADDR_DIGIT_0		= 0x01 ,
+	ADDR_DIGIT_1 		= 0x02 ,
+	ADDR_DIGIT_2 		= 0x03 ,
+	ADDR_DIGIT_3 		= 0x04 ,
+	ADDR_DIGIT_4 		= 0x05 ,
+	ADDR_DIGIT_5 		= 0x06 ,
+	ADDR_DIGIT_6 		= 0x07 ,
+	ADDR_DIGIT_7		= 0x08 ,
+	ADDR_DECODE_MODE 	= 0x09 ,
+	ADDR_INTENSITY 		= 0x0A ,
+	ADDR_SCAN_LIMIT 	= 0x0B ,
+	ADDR_SHUTDOWN		= 0x0C ,
+	ADDR_EMPTY			= 0x0D ,
+	ADDR_DISPLAY_TEST	= 0x0F
+}	Register_Address_Map;
+/***************************************************************************************/
 
 void _max7219_print_one_digit(max7219_struct max7219_handler, uint8_t position, uint32_t value);
 void _max7219_write_strob(max7219_struct max7219_handler);
@@ -13,65 +32,54 @@ void _max7219_push_data(max7219_struct max7219_handler);
 
 /***************************************************************************************/
 
-void _max7219_push_data(max7219_struct max7219_handler)
-{
-    HAL_SPI_Transmit(max7219_handler.spi, max7219_handler.data, BYTE_IN_SPI_PACKAGE, SPI_PACKAGE_TIMEOUT);
-    _max7219_write_strob(max7219_handler);
-}
-
-void max7219_init(max7219_struct *max7219_handler)
+void max7219_init(max7219_struct *max7219_handler, max7219_Decode_Mode _decodemode,  max7219_LED_Intensity _intensity, max7219_Scan_Limit _scanlimit, max7219_Shutdown _shutdown)
 {
 	// test - On
-	max7219_handler->data[0] = 0x0F;  max7219_handler->data[1] = 0x01;
-	max7219_handler->data[2] = 0x0F;  max7219_handler->data[3] = 0x01;
-	max7219_handler->data[4] = 0x0F;  max7219_handler->data[5] = 0x01;
-	max7219_handler->data[6] = 0x0F;  max7219_handler->data[7] = 0x01;
+	max7219_handler->data[0] = ADDR_DISPLAY_TEST;  max7219_handler->data[1] = TestMode;
+	max7219_handler->data[2] = ADDR_DISPLAY_TEST;  max7219_handler->data[3] = TestMode;
+	max7219_handler->data[4] = ADDR_DISPLAY_TEST;  max7219_handler->data[5] = TestMode;
+	max7219_handler->data[6] = ADDR_DISPLAY_TEST;  max7219_handler->data[7] = TestMode;
 	_max7219_push_data(*max7219_handler);
 	HAL_Delay(1500);
 
 	// test - Off
-	max7219_handler->data[0] = 0x0F;  max7219_handler->data[1] = 0x00;
-	max7219_handler->data[2] = 0x0F;  max7219_handler->data[3] = 0x00;
-	max7219_handler->data[4] = 0x0F;  max7219_handler->data[5] = 0x00;
-	max7219_handler->data[6] = 0x0F;  max7219_handler->data[7] = 0x00;
+	max7219_handler->data[0] = ADDR_DISPLAY_TEST;  max7219_handler->data[1] = WorkMode;
+	max7219_handler->data[2] = ADDR_DISPLAY_TEST;  max7219_handler->data[3] = WorkMode;
+	max7219_handler->data[4] = ADDR_DISPLAY_TEST;  max7219_handler->data[5] = WorkMode;
+	max7219_handler->data[6] = ADDR_DISPLAY_TEST;  max7219_handler->data[7] = WorkMode;
 	_max7219_push_data(*max7219_handler);
 
-	// Decode Mode - No 1 in 1
-	// myTrans[1] hex   -> FF
-	// myTrans[1] pixel -> 00
-	max7219_handler->data[0] = 0x09;  max7219_handler->data[1] = 0x00;
-	max7219_handler->data[2] = 0x09;  max7219_handler->data[3] = 0x00;
-	max7219_handler->data[4] = 0x09;  max7219_handler->data[5] = 0x00;
-	max7219_handler->data[6] = 0x09;  max7219_handler->data[7] = 0x00;
+	// Decode Mode - No. 1 in 1
+	max7219_handler->data[0] = ADDR_DECODE_MODE;  max7219_handler->data[1] = _decodemode;
+	max7219_handler->data[2] = ADDR_DECODE_MODE;  max7219_handler->data[3] = _decodemode;
+	max7219_handler->data[4] = ADDR_DECODE_MODE;  max7219_handler->data[5] = _decodemode;
+	max7219_handler->data[6] = ADDR_DECODE_MODE;  max7219_handler->data[7] = _decodemode;
 	_max7219_push_data(*max7219_handler);
 
 	// Intensity 3/32
-	max7219_handler->data[0] = 0x0A;  max7219_handler->data[1] = 0x01;
-	max7219_handler->data[2] = 0x0A;  max7219_handler->data[3] = 0x01;
-	max7219_handler->data[4] = 0x0A;  max7219_handler->data[5] = 0x01;
-	max7219_handler->data[6] = 0x0A;  max7219_handler->data[7] = 0x01;
+	max7219_handler->data[0] = ADDR_INTENSITY;  max7219_handler->data[1] = _intensity;
+	max7219_handler->data[2] = ADDR_INTENSITY;  max7219_handler->data[3] = _intensity;
+	max7219_handler->data[4] = ADDR_INTENSITY;  max7219_handler->data[5] = _intensity;
+	max7219_handler->data[6] = ADDR_INTENSITY;  max7219_handler->data[7] = _intensity;
 	_max7219_push_data(*max7219_handler);
 
 	//Scan Limit - All
-	max7219_handler->data[0] = 0x0B;  max7219_handler->data[1] = 0x07;
-	max7219_handler->data[2] = 0x0B;  max7219_handler->data[3] = 0x07;
-	max7219_handler->data[4] = 0x0B;  max7219_handler->data[5] = 0x07;
-	max7219_handler->data[6] = 0x0B;  max7219_handler->data[7] = 0x07;
+	max7219_handler->data[0] = ADDR_SCAN_LIMIT;  max7219_handler->data[1] = _scanlimit;
+	max7219_handler->data[2] = ADDR_SCAN_LIMIT;  max7219_handler->data[3] = _scanlimit;
+	max7219_handler->data[4] = ADDR_SCAN_LIMIT;  max7219_handler->data[5] = _scanlimit;
+	max7219_handler->data[6] = ADDR_SCAN_LIMIT;  max7219_handler->data[7] = _scanlimit;
 	_max7219_push_data(*max7219_handler);
 
 	// Shutdown - none
 	// myTrans[1] -> 00 sleep
 	// myTrans[1] -> 01 work
-	max7219_handler->data[0] = 0x0C;  max7219_handler->data[1] = 0x01;
-	max7219_handler->data[2] = 0x0C;  max7219_handler->data[3] = 0x01;
-	max7219_handler->data[4] = 0x0C;  max7219_handler->data[5] = 0x01;
-	max7219_handler->data[6] = 0x0C;  max7219_handler->data[7] = 0x01;
+	max7219_handler->data[0] = ADDR_SHUTDOWN;  max7219_handler->data[1] = _shutdown;
+	max7219_handler->data[2] = ADDR_SHUTDOWN;  max7219_handler->data[3] = _shutdown;
+	max7219_handler->data[4] = ADDR_SHUTDOWN;  max7219_handler->data[5] = _shutdown;
+	max7219_handler->data[6] = ADDR_SHUTDOWN;  max7219_handler->data[7] = _shutdown;
 	_max7219_push_data(*max7219_handler);
 
 }
-/***************************************************************************************/
-
-
 /***************************************************************************************/
 
 void _max7219_print_one_digit(max7219_struct max7219_handler, uint8_t position, uint32_t value)
@@ -81,6 +89,13 @@ void _max7219_print_one_digit(max7219_struct max7219_handler, uint8_t position, 
 	spi_buffer[1] = value	; // znachenie
 	HAL_SPI_Transmit(max7219_handler.spi, spi_buffer, BYTE_IN_SPI_PACKAGE, SPI_PACKAGE_TIMEOUT);
 	_max7219_write_strob(max7219_handler);
+}
+/***************************************************************************************/
+
+void _max7219_push_data(max7219_struct max7219_handler)
+{
+    HAL_SPI_Transmit(max7219_handler.spi, max7219_handler.data, BYTE_IN_SPI_PACKAGE, SPI_PACKAGE_TIMEOUT);
+    _max7219_write_strob(max7219_handler);
 }
 /***************************************************************************************/
 
--- max7219_4x_dot_sm.h
+++ max7219_4x_dot_sm.h
@@ -8,9 +8,71 @@ typedef struct
     uint16_t 			 cs_pin;
     uint8_t				data[8];
 } max7219_struct;
+/***********************************/
 
+typedef enum
+{
+	DisplayDigit_0_only	=	0x00,
+	DisplayDigit_0_1	=	0x01,
+	DisplayDigit_0_2	=	0x02,
+	DisplayDigit_0_3	=	0x03,
+	DisplayDigit_0_4	=	0x04,
+	DisplayDigit_0_5	=	0x05,
+	DisplayDigit_0_6	=	0x06,
+	DisplayDigit_0_7	=	0x07
+}	 max7219_Scan_Limit;
+/***********************************/
 
-void max7219_init(max7219_struct *max7219_handler);
+typedef enum
+{
+	Intensity_1		= 0x00,
+	Intensity_3		= 0x01,
+	Intensity_5		= 0x02,
+	Intensity_7		= 0x03,
+	Intensity_9		= 0x04,
+	Intensity_11	= 0x05,
+	Intensity_13	= 0x06,
+	Intensity_15	= 0x07,
+	Intensity_17	= 0x08,
+	Intensity_19	= 0x09,
+	Intensity_21	= 0x0A,
+	Intensity_23	= 0x0B,
+	Intensity_25	= 0x0C,
+	Intensity_27	= 0x0D,
+	Intensity_29	= 0x0E,
+	Intensity_31	= 0x0F
+}	 max7219_LED_Intensity;
+/***********************************/
+
+typedef enum
+{
+	ShutdownMode	= 0,
+	NormalOperation	= 1
+}	max7219_Shutdown;
+/***********************************/
+
+typedef enum
+{
+	WorkMode	= 0,
+	TestMode	= 1
+}	max7219_Display_Test ;
+/***********************************/
+
+typedef enum
+{
+	NoDecode			= 0x00,
+	DecodeForDigit_0	= 0x01,
+	DecodeForDigit_1_0	= 0x03,
+	DecodeForDigit_2_0	= 0x07,
+	DecodeForDigit_3_0	= 0x0F,
+	DecodeForDigit_4_0	= 0x1F,
+	DecodeForDigit_5_0	= 0x3F,
+	DecodeForDigit_6_0	= 0x7F,
+	DecodeForDigit_7_0	= 0xFF,
+}	 max7219_Decode_Mode;
+/***********************************/
+
+void max7219_init(max7219_struct *max7219_handler, max7219_Decode_Mode _decodemode,  max7219_LED_Intensity _intensity, max7219_Scan_Limit _scanlimit, max7219_Shutdown _shutdown);
+/***********************************/
 
 #endif 	//	MAX7219_4X_DOT_SM_H_INCLUDED
-
